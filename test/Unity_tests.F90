c3d8fabcc834aab844ccca9fa61fba8a8dc421a6
deniseiras <denis.eiras@gmail.com>
2023-02-28 14:57:43 -0300

    test 2

--- test/Unity_tests.F90
+++ test/Unity_tests.F90
@@ -63,7 +63,7 @@ program Unity_tests
       test_result = .true.
 
       ! Initialize two data objects
-      dat_a%x = 2
+      dat_a%x = 1
 
       ! Initialize the list with dat_a
       ptr%p => dat_a
